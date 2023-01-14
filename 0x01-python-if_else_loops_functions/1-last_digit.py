8bca959178007651771237f627125fc430d9af84
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-14 12:32:39 +0000

    Made corrections on file 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import math
 import ctypes
 import random
 number = random.randint(-10000, 10000)
@@ -8,10 +9,10 @@ num = id(number)  # This stores the memory address of number.
 a = ctypes.cast(num, ctypes.py_object).value
 # This retrieve the value from the num
 
-b = int(repr(a)[-1])  # This prints the last digit of the value
+b = int (math.fmod(a, 10)) # This prints the last digit of the value
 
-if (b < 6 and b != 0):
-    print("Last digit of", number, "is", b, "and is less than 6 and not 0")
+if (b < 6):
+    print("Last digit of", number, "is", b,"and is less than 6 and not 0")
 if (b == 0):
     print("Last digit of", number, "is", b, "and is 0")
 if (b > 5):
