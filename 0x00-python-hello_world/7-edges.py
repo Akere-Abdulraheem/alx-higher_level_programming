1242006b399aba5cc3b746e6f5e201efd4a406d2
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-16 12:41:53 +0000

    file 7 created

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]  # stores the first 3 letters
+word_last_2 = word[7:]  # stores the last 2 letters
+Middle_word = word[1:8]  # prints all letters excluding first and last letters
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {Middle_word}")
