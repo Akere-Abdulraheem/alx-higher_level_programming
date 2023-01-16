5082a4912de0080907af3c7e3a558464e8373f39
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-16 12:00:07 +0000

    Done with file 5

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str * 3) # print the value of str thrice
+print(str * 3)  # print the value of str thrice
 print(str[0:9])  # prints the first eight characters of str
