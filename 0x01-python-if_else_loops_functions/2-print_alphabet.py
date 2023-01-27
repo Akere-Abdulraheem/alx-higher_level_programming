8b734aa5f862cbce901bc4c03790f80d59ca70e2
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-27 16:25:40 +0000

    ...

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,13 +3,12 @@
 #  Printing ASCII alphabet in lowercase with no new line
 #  .....................RULES...........................
 #  One print function with .format()
-#  one loop....used while loop
+#  one loop....used a for loop
 #  characters can't be stored in a variable
 #  no import
 
 # a = ord('a')
-for x in range(97,122):
+for x in range(97, 122):
     b = chr(x)
     c = "{}".format(b)
     print(c)
-#    a += 1
