172c726cb0b6901ae90bd51371399e677accb768
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-27 16:18:05 +0000

    added comments

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+'''
+Printing ASCII alphabet in lowercase with no new line
+.....................RULES...........................
+One print function with .format()
+one loop....used while loop
+characters can't be stored in a variable
+no import
+'''
+
 a = ord('a')
 while a <= ord('z'):
     b = chr(a)
