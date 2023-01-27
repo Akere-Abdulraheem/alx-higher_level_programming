687587daadb25a4ff26f4da55ab7bcbcb15ab2d1
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-27 16:14:23 +0000

    corrections

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 a = ord('a')
-while a <= ord ('z'):
-	b = chr(a)
-	c = "{}".format(b)
-	print(c)
-	a += 1
+while a <= ord('z'):
+    b = chr(a)
+    c = "{}".format(b)
+    print(c)
+    a += 1
