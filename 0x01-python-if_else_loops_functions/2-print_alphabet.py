124ac4368479ac02e5f1f5e601cca7301ebd1182
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-27 16:20:19 +0000

    ...

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-'''
-Printing ASCII alphabet in lowercase with no new line
-.....................RULES...........................
-One print function with .format()
-one loop....used while loop
-characters can't be stored in a variable
-no import
-'''
+#  Printing ASCII alphabet in lowercase with no new line
+#  .....................RULES...........................
+#  One print function with .format()
+#  one loop....used while loop
+#  characters can't be stored in a variable
+#  no import
 
 a = ord('a')
 while a <= ord('z'):
