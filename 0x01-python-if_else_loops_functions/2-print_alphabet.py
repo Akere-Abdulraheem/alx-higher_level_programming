009717b7a3ac0a641be299b12250b10d4272c932
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-27 16:24:43 +0000

    ...

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -7,9 +7,9 @@
 #  characters can't be stored in a variable
 #  no import
 
-a = ord('a')
-while a <= ord('z'):
-    b = chr(a)
+# a = ord('a')
+for x in range(97,122):
+    b = chr(x)
     c = "{}".format(b)
     print(c)
-    a += 1
+#    a += 1
