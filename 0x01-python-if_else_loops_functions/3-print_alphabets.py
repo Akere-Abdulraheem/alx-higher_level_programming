e4cbcef54f3207e777ea28b7b1eaefef613092c7
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-27 16:42:55 +0000

    ..

--- 0x01-python-if_else_loops_functions/3-print_alphabets.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabets.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-#  Printing ASCII alphabet in lowercase with no new line
-#  .....................RULES...........................
+#  Printing ASCII alphabet in lowercase except q and e with no new line
+#  .............................RULES..................................
 #  One print function with .format()
 #  one loop
 #  characters can't be stored in a variable
 #  no import
-#  end="" --- allows me to print without a new line in py
 
 for x in range(97, 122):
     c = "{}".format(chr(x))
