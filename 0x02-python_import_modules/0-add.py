289e21e2334cc337fe498c3379c28ba07ac0d276
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-28 16:41:48 +0000

    Let hope it works as intended

--- 0x02-python_import_modules/0-add.py
+++ 0x02-python_import_modules/0-add.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 
-def add(a, b):
+'''
+To write a program thats imports the function 'add'
+........................RULES.....................
+print with .format()
+assign values to variable(a and b)
+programs displays 'value a + value b = addition of a and b'
+CAN ONE USE ADD_0 once
+no __import__ or *
+code shouldn't run when imported using __import__
+'''
 
-    add = a + b
-a = 1, b = 2
-print("%d", add()) 
+# CODE...
+# def add is the function which as arg: a and b and return the addition
+# a and b are the var which store values
+# var d store the f-format
+
+
+def add(a, b):
+    return a + b
+
+
+a = 1
+b = 2
+d = "{0} + {1} = {2}".format(a, b, add(a, b))
+print(d)
