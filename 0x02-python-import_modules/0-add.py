c6d2d33bf2099e807699c0fecf8ba5b928cf7098
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-01-29 00:12:28 +0000

    ...2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-'''
-To write a program thats imports the function 'add' from file 'add_0.py'
-........................RULES.....................
-print with .format()
-assign values to variable(a and b)
-programs displays 'value a + value b = addition of a and b'
-CAN ONE USE ADD_0 once
-no __import__ or *
-code shouldn't run when imported using __import__
-'''
+
+#  To write a program thats imports the function 'add' from file 'add_0.py'
+#  ........................RULES.....................
+#  print with .format()
+#  assign values to variable(a and b)
+#  programs displays 'value a + value b = addition of a and b'
+#  CAN ONE USE ADD_0 once
+#  no __import__ or *
+#  code shouldn't run when imported using __import__
 
 # CODE...
 # def add is the function which as arg: a and b and return the addition
