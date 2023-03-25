2bcdce07af2ee6265be64254b5057184b953174d
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-25 23:17:09 +0000

    Came back to python after researching

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-
 #  To write a program thats imports the function 'add' from file 'add_0.py'
+
 #  ........................RULES.....................
 #  print with .format()
 #  assign values to variable(a and b)
@@ -13,28 +13,11 @@
 # CODE...
 # def add is the function which as arg: a and b and return the addition
 # a and b are the var which store values
-# var d store the f-format
+# var c stores the function
 
-
-# def add(a, b):
-#    """My addition function
-#
-#    Args:
-#        a: first integer
-#        b: second integer
-#
-#    Returns:
-#        The return value. a + b
-#    """
-#    return (a + b)
-
-import sys
-from add_0 import add
-
-if __name__ == "__main__"  # This line does not allow the code to be
-# executed when imported
-
+import add_0 as addition
 a = 1
-b = 2
-d = "{0} + {1} = {2}".format(a, b, c(a, b))
-print(d)
+b = 5
+c = addition.add(a, b)
+# print(f"{a} + {b} = {c}")
+print("{0} + {1} = {2}".format(a, b, c))
