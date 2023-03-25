5132049891375d0bd5c8f953e905a04dae556f04
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-25 23:51:49 +0000

    added "from add_0"

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as addition
+from add_0 import add as addition
 if __name__ == "__main__":
     a = 1
     b = 2
-    c = addition.add(a, b)
+    c = add(a, b)
     print("{0} + {1} = {2}".format(a, b, c))
