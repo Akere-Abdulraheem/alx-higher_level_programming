df044f3931a5fa526723f61fdeb88bdfa4f25b7d
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-26 11:16:13 +0000

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Write a program that imports the variable a from the 
+# Write a program that imports the variable a from the
 # file variable_load_5.py and prints its value.
 
 from variable_load_5 import a
