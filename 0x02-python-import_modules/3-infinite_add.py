59bb2cdc493f15f6796917b1146706b5b4778f46
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-26 10:50:07 +0000

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv  # sys.argv == argv and argc in C
+
+n = len(argv)  # total arg
+sum = 0  # initalize addition to zero
+
+for a in range(1, n):
+    sum += int(argv[a])
+print(sum)  # prints sum of all args
