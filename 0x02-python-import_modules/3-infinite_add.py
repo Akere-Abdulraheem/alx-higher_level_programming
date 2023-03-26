b1f59f81f2ce938cfdb6d0930f03d2e7bc885cc9
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-26 10:52:20 +0000

    prevents module from auto run - Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,7 @@ from sys import argv  # sys.argv == argv and argc in C
 n = len(argv)  # total arg
 sum = 0  # initalize addition to zero
 
-for a in range(1, n):
-    sum += int(argv[a])
+if __name__ == "__main__":
+   for a in range(1, n):
+       sum += int(argv[a])
 print(sum)  # prints sum of all args
