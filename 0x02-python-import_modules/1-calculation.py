7ef846bc79801f283a1d50f65082a019c618f8e3
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-26 00:12:53 +0000

    Done with task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,36 +1,13 @@
 #!/usr/bin/python3
-
-'''
-To write a program thats imports the function 'add' from file 'add_0.py'
-........................RULES.....................
-print with .format()
-assign values to variable(a and b)
-programs displays 'value a + value b = addition of a and b'
-CAN ONE USE ADD_0 once
-no __import__ or *
-code shouldn't run when imported using __import__
-'''
-
-# CODE...
-# def add is the function which as arg: a and b and return the addition
-# a and b are the var which store values
-# var d store the f-format
-
-
-#def add(a, b):
-#    """My addition function
-#
-#    Args:
-#        a: first integer
-#	b: second integer
-#
-#    Returns:
-#        The return value. a + b
-#    """
-#    return (a + b)
-
-import calculator_1
-a = 10
-b = 5
-d = "{0} + {1} = {2}".format(a, b, calculator_1(a, b))
-print(d)
+from calculation_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+    print("{0} + {1} = {2}".format(a, b, c))
+    print("{0} - {1} = {2}".format(a, b, d))
+    print("{0} * {1} = {2}".format(a, b, e))
+    print("{0} / {1} = {2}".format(a, b, f))
