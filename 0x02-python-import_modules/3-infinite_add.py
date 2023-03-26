8c9e3a7c86782e7ee18b3641b71128bea0fa3e1e
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-26 10:53:41 +0000

    py styling code - Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ n = len(argv)  # total arg
 sum = 0  # initalize addition to zero
 
 if __name__ == "__main__":
-   for a in range(1, n):
-       sum += int(argv[a])
+    for a in range(1, n):
+        sum += int(argv[a])
 print(sum)  # prints sum of all args
