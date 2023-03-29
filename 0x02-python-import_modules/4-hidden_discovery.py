1bcc4e17f2525d398e558de9d09104dde4a730ee
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-03-29 14:18:44 +0000

    autoupdate-Backup

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# Write a program that prints all the names defined by the compiled module
+
+if __name__ == "__main__":
--- 0x0D-SQL_introduction/README.md
+++ 0x0D-SQL_introduction/README.md
@@ -1,1 +1,3 @@
 # SQL-Structured Query Language- introduction
+
+This directory contains scripts useful in sql from cli
