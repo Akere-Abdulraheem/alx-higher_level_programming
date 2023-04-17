a12e59a810679c130e5c73937d8080c11b4d9bd0
Akere-Abdulraheem <akereodunayo@gmail.com>
2023-04-17 22:11:25 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,24 +5,24 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-# Variables
-  username = argv[1]  #  Mysql username
-  password =argv[2]   #  Mysql password
-  db_name = argv[3]   #  Database Name
+    # Variables
+    username = argv[1]  # Mysql username
+    password = argv[2]   # Mysql password
+    db_name = argv[3]   # Database Name
 
 # Connection setup to mysqldb
-  db = MySQLdb.connect(
-		 host="localhost",
-		 port=3306,
-		 user=username,
-		 passwd=password,
-		 db=db_name,)
+    db = MySQLdb.connect(
+                      host="localhost",
+                      port=3306,
+                      user=username,
+                      passwd=password,
+                      db=db_name,)
 # port 3306 is the default port for classic mysql protocol
 
 # getting a cursor in mysql python
 mycursor = db.cursor()
 
-#The commands to be executed in mysql
+# The commands to be executed in mysql
 sql = "SELECT * FROM states ORDER by id;"
 
 mycursor.execute(sql)
@@ -31,9 +31,8 @@ mycursor.execute(sql)
 res = mycursor.fetchall()
 # fetchall fetches all rows from last executed command
 
-for res in res: # res in res allows it to print on a new line
-  print(res)
+for res in res:  # res in res allows it to print on a new line
+    print(res)
 
 mycursor.close()
 db.close()
-
